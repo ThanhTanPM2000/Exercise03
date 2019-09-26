588c44d1380480c9ffc22603cd17a6bce6b4cc47
ThanhTanNguyen2007 <tan.187pm20569@vanlanguni.vn>
2019-09-26 12:40:01 +0700

    Đây là code về for loop

---
+++ Exercise03/Exercise03/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            VeCheo(4, 'k');
+            Console.ReadLine();
+
+        }
+        static void VeNgang(int n, char c)
+        {
+
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.Write(c);
+            }
+        }
+        static void VeDoc(int n, char c)
+        {
+
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine("*");
+            }
+        }
+        static void VeCheo(int n, char c)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                VeNgang(i, ' ');
+                Console.WriteLine(c);
+            }
+        }
+
+
+    }
+}
+
